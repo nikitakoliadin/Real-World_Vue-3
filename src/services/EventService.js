815842e62f1dc80a38a6c7fa45ff8041b1c7a6c1
Nikita Koliadin <qThegamEp@gmail.com>
2022-10-27 09:44:29 +0300

    Lesson 6 start

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -10,7 +10,7 @@ const apiClient = axios.create({
 })
 
 export default {
-    getEvents() {
-        return apiClient.get('/events')
-    }
+  getEvents() {
+    return apiClient.get('/events')
+  }
 }
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="events">
-    <h1>Events For Good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService'
+import EventService from '@/services/EventService.js'
 
 export default {
   name: 'EventList',
